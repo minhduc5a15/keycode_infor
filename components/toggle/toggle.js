d67148d31590b88b2db3fe3a108fc8616aa5bb41
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-08-04 21:25:58 +0700

    Delete components/toggle directory

--- components/toggle/toggle.js
+++
@@ -1,9 +0,0 @@
-import styles from "../../styles/styles";
-const Toggle = ({ toggleTheme, theme }) => {
-     return (
-          <button onClick={toggleTheme} className={styles["button-theme"]}>
-               {theme}
-          </button>
-     );
-};
-export default Toggle;
