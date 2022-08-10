8567de1d9c05773142976ae2cc0aa14ebabe952d
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-08-11 00:26:20 +0700

    Update head.js

--- components/head/head.js
+++ components/head/head.js
@@ -5,6 +5,7 @@ const Box = () => {
                <title>Key Code</title>
                <meta name="viewport" content="width=device-width, initial-scale=1" />
                <meta charSet="utf-8" />
+               <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>%E2%8C%A8%EF%B8%8F</text></svg>" />
           </Head>
      );
 };
