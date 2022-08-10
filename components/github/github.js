2b7411a82f27eed47f47775e2207efeeacfe2246
minhduc5a15 <104719252+minhduc5a15@users.noreply.github.com>
2022-08-11 00:50:16 +0700

    Add files via upload

---
+++ components/github/github.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Button from "@mui/material/Button";
+const Link = styled(Button)`
+     position: absolute;
+     left: 0;
+     bottom: 0;
+     width: 80px;
+     height: 40px;
+     background: ${({ theme }) => theme.background};
+     color: ${({ theme }) => theme.text};
+     transition: 0.5s ease-in-out;
+     border-radius: 0;
+     &:hover {
+          background: ${({ theme }) => theme.hover};
+          color: ${({ theme }) => theme.background};
+     }
+`;
+const GithubLink = () => {
+     return (
+          <Link href="https://github.com/minhduc5a15">
+               <GitHubIcon />
+          </Link>
+     );
+};
+export default GithubLink;
